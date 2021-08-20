8fd7297e1be5680534e797c51f93f3f2f9fae80b
thebeautycoder <janaehenry00@gmail.com>
2021-08-20 14:37:23 -0400

    test

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -1,14 +1,69 @@
 class TaskManager {
   constructor(){
+
   this.tasks = [];
+  currentId = 0;
+  currentId = this.currentId();
+  TaskManager.addTask('name', 'description', 'assignedTo', 'dueDate', 'status')
+  this.currentId++
 }
+/* addTask {
+  constructor(name, description, assignedTo, dueDate, status) {
+    this.currentId = addTask;
+   
+  }
+}
+this.tasks.push*/
 
 
 
 
 
 
+const task1 = {
+  id: 1,
+  name: 'Food Website',
+  description: 'Create a responsive website for fast food chain Ronalds',
+  assignedTo: 'Raul Ramirez',
+  dueDate: '2021-08-20',
+  status: 'IN PROGRESS'
+};
 
+const task2 = {
+  id: 2,
+  name: 'Form Correction',
+  description: "Debug form on McNeil's landing page",
+  assignedTo: 'Jasmine Patel',
+  dueDate: '2021-09-20',
+  status: 'DONE'
+};
+
+const task3 = {
+  id: 3,
+  name: 'Create Web Banner',
+  description: 'Create web banner for Yosemite Printing',
+  assignedTo: 'Manny Wyler',
+  dueDate: '2021-02-20',
+  status: 'TODO'
+};
+
+const task4 = {
+  id: 4,
+  name: 'Open House Ad',
+  description: 'Add tables and form with bootstrap',
+  assignedTo: 'Danny Caruso',
+  dueDate: '2021-08-20',
+  status: 'REVIEW'
+};
+
+const task5 = {
+  id: 5,
+  name: 'Link CSS File',
+  description: 'link CSS file and add other assets to repository',
+  assignedTo: 'Michael Corleone',
+  dueDate: '2021-10-15',
+  status: 'IN PROGRESS'
+};
 
 
 
